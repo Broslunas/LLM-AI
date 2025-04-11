2b805418f7fb1655ebc74e9447cae8424d4114e8
Broslunas <pablo@broslunas.com>
2025-04-11 17:09:41 +0100

    feat: Add user and total statistics display with API integration in stats page

---
+++ src/pages/api/stats.ts
@@ -0,0 +1,66 @@
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
+
+config();
+
+const MONGO_URI = process.env.MONGO_URI || "";
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is not defined in the environment variables.");
+}
+
+const client = new MongoClient(MONGO_URI);
+
+export const prerender = false;
+
+export async function POST({ request }: { request: Request }) {
+  try {
+    const body = await request.json();
+    const { username } = body;
+
+    await client.connect();
+    const db = client.db("broslunas");
+    const usersCollection = db.collection("users");
+
+    const totalUsers = await usersCollection.countDocuments();
+    const totalMessages = await usersCollection
+      .aggregate([{ $group: { _id: null, total: { $sum: "$messageCount" } } }])
+      .toArray();
+
+    let userStats = {
+      username: "Desconocido",
+      email: "Desconocido",
+      userMessages: 0,
+    };
+    if (username) {
+      const user = await usersCollection.findOne({ username });
+      if (user) {
+        userStats = {
+          username: user.username,
+          email: user.email,
+          userMessages: user.messageCount || 0,
+        };
+      }
+    }
+
+    return new Response(
+      JSON.stringify({
+        ...userStats,
+        totalUsers,
+        totalMessages: totalMessages[0]?.total || 0,
+      }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    );
+  } catch (error) {
+    console.error("Error fetching stats:", error);
+    return new Response(JSON.stringify({ response: "Error fetching stats." }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  } finally {
+    await client.close();
+  }
+}
+
+export default {
+  POST,
+};
